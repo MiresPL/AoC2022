bcb2df55335969355e88997593974b43675774a0
MiresPL <mateusz.skorek@tm1.edu.pl>
2022-12-06 15:17:31 +0100

    Day5 Part 1 & Part 2

---
+++ src/main/java/com/mires/Day5/Day5.java
@@ -0,0 +1,117 @@
+package com.mires.Day5;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class Day5 {
+    public static void main(String[] args) {
+        final List<String> inputArray = new ArrayList<>();
+        final Map<Integer, Stack> stackMap = new HashMap<>();
+        try {
+            final File inputFile = new File("C:\\Users\\Mateusz\\IdeaProjects\\WŁASNE PROJEKTY\\AdventOfCode2022\\AoC2022\\src\\main\\java\\com\\mires\\Day5\\input.txt");
+            final Scanner scanner = new Scanner(inputFile);
+
+            while (scanner.hasNextLine()) {
+                inputArray.add(scanner.nextLine());
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 0; i < 9; i++) {
+            stackMap.put(i, new Stack());
+        }
+
+
+        for (String s : inputArray) {
+            if (s.isEmpty() || s.contains(" 1   2   3")) break;
+            s += " ";
+
+            final List<String> toAdd = new ArrayList<>();
+            for (int i = 0; i < s.length(); i+=4) {
+                toAdd.add(s.substring(i, i + 4));
+            }
+
+            if (toAdd.size() < stackMap.size()) {
+                for (int i = 0; i < stackMap.size() - toAdd.size(); i++) {
+                    toAdd.add(" ");
+                }
+            }
+
+            for (int i = 0; i < toAdd.size(); i++) {
+                if (toAdd.get(i).replaceAll(" ", "").isEmpty()) continue;
+                stackMap.get(i).addCreateFirst(toAdd.get(i).replaceAll(" ", ""));
+            }
+
+            for (int i = 0; i < stackMap.size(); i++) {
+                stackMap.get(i).setTopCreate();
+            }
+
+        }
+
+
+        for (String s : inputArray) {
+            if (!s.contains("move")) continue;
+            final String[] split = s.split(" ");
+            final int amount = Integer.parseInt(split[1]);
+            final int from = Integer.parseInt(split[3]);
+            final int to = Integer.parseInt(split[5]);
+
+            for (int i = 0; i < amount; i++) {
+                stackMap.get(to - 1).addCreate(stackMap.get(from - 1).removeCreate());
+            }
+        }
+        System.out.print("Top Crates: ");
+        for (int i = 0; i < stackMap.size(); i++) {
+            System.out.print(stackMap.get(i).topCreate.replaceAll(" ", "").replace("[", "").replace("]", ""));
+        }
+
+    }
+
+
+    protected static class Stack {
+        private String topCreate = "";
+        private List<String> creates;
+
+        public Stack() {
+            this.creates = new ArrayList<>();
+        }
+
+        public void addCreateFirst(String create) {
+            this.creates.add(create);
+        }
+
+        public void addCreate(final String create) {
+            this.topCreate = create;
+            this.creates.add(0, create);
+        }
+
+        public String removeCreate() {
+            final String create = this.topCreate;
+            this.creates.remove(0);
+            if (this.creates.size() > 0) {
+                this.topCreate = this.creates.get(0);
+            } else {
+                this.topCreate = "";
+            }
+            return create;
+        }
+
+        public void setTopCreate() {
+            if (this.creates.isEmpty()) return;
+            this.topCreate = this.creates.get(0);
+        }
+
+        @Override
+        public String toString() {
+            final StringBuilder sb = new StringBuilder();
+            sb.append("Top Create: ").append(this.topCreate).append("\n");
+            for (int i = 0; i < this.creates.size(); i++) {
+                sb.append(i).append(". ").append(this.creates.get(i)).append("\n");
+            }
+            return sb.toString();
+        }
+    }
+}
